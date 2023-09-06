fc31ae510d6625747ae0e35312028b238c2200ca
SumanthFSD <sumanthreddy83@gmail.com>
2023-09-06 12:18:25 +0530

    Delete Post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 // var popup = require('popups');
 
 module.exports.create = function(req, res){
@@ -21,5 +22,19 @@ module.exports.create = function(req, res){
     }
 
     return res.redirect('back');
-
 }
+
+module.exports.destroy = function(req,res){
+    Post.findById(req.params.id, function(err, post){
+        // .id means converting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
+}
+
